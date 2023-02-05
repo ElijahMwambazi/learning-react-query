073e00a78d60016266d26a4eed3ecd81daa893c9
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-02-05 21:47:05 +0200

    Implemented fetching of comments

--- blog-em-ipsum/src/components/PostDetail.tsx
+++ blog-em-ipsum/src/components/PostDetail.tsx
@@ -1,4 +1,7 @@
-import { Post } from "../api/services/Blog.services";
+import {
+  Post,
+  useFetchComments,
+} from "../api/services/Blog.services";
 
 export type PostDetailProps = {
   post: Post;
@@ -7,23 +10,39 @@ export type PostDetailProps = {
 const PostDetail = ({
   post,
 }: PostDetailProps) => {
-  // replace with useQuery
-  const data: Post[] = [];
+  const { data, isLoading, isError } =
+    useFetchComments(post.id);
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>
-        {post!.title}
-      </h3>
-      <button>Delete</button>{" "}
-      <button>Update title</button>
-      <p>{post!.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment!.id}>
-          {comment!.email}: {comment!.body}
-        </li>
-      ))}
+      <div className="post-details-header">
+        <h3 className="post-detail-title">
+          {post!.title}
+        </h3>
+        <div className="post-detail-buttons">
+          <button>Update title</button>
+          <button>Delete</button>{" "}
+        </div>
+      </div>
+      <p className="post-detail-text">
+        {post!.body}
+      </p>
+      <h4 className="post-detail-comments-header">
+        Comments
+      </h4>
+      <ul className="comments">
+        {isLoading && <h3>...Loading</h3>}
+        {isError && <h3>Error fetching data</h3>}
+        {data &&
+          data.data.map(
+            ({ id, name, email, body }) => (
+              <li key={id}>
+                <span>{name}</span> ({email}):{" "}
+                {body}
+              </li>
+            )
+          )}
+      </ul>
     </>
   );
 };
