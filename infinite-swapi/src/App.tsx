cb3e1cba97a4e38dc35ef28b163dd8a822ad7496
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-02-06 21:36:27 +0200

    Added react query provider and devtools

--- infinite-swapi/src/App.tsx
+++ infinite-swapi/src/App.tsx
@@ -1,12 +1,55 @@
+import {
+  Suspense,
+  lazy,
+  useEffect,
+  useState,
+} from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./App.css";
 
+declare global {
+  interface Window {
+    toggleDevtools: any;
+  }
+}
+
 const App = () => {
+  const queryClient = new QueryClient();
+
+  const ReactQueryDevtoolsProduction = lazy(() =>
+    import(
+      "@tanstack/react-query-devtools/build/lib/index.prod.js"
+    ).then((d) => ({
+      default: d.ReactQueryDevtools,
+    }))
+  );
+
+  const [showDevtools, setShowDevtools] =
+    useState(false);
+
+  useEffect(() => {
+    window.toggleDevtools = () =>
+      setShowDevtools((old) => !old);
+  });
+
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      {/* <InfinitePeople /> */}
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        {/* <InfinitePeople /> */}
+        {/* <InfiniteSpecies /> */}
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
+    </QueryClientProvider>
   );
 };
 
