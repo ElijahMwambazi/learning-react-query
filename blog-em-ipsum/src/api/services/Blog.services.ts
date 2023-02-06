2f638948c6750b022679b0e107c4cea473077cee
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-02-06 10:26:50 +0200

    Added usePostsPrefetch hook

--- blog-em-ipsum/src/api/services/Blog.services.ts
+++ blog-em-ipsum/src/api/services/Blog.services.ts
@@ -1,7 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import {
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import axios from "axios";
-
-type ValueOf<T> = T[keyof T];
+import {
+  CurrentPage,
+  maxPostPage,
+} from "../../components/Posts";
+import { ValueOf } from "../../utils/types.utils";
 
 export type Post = {
   id: number;
@@ -27,55 +34,82 @@ export type CommentQueryData = {
 
 export type PostId = ValueOf<Pick<Post, "id">>;
 
+const fetchPosts = async (
+  pageNumber: CurrentPage
+) =>
+  await axios.get<PostsQueryData, any>(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
+  );
+
 export const useFetchPosts = (
-  pageNumber: number
+  pageNumber: CurrentPage
 ) => {
   return useQuery<PostsQueryData>(
     ["posts", pageNumber],
-    async () =>
-      await axios.get<PostsQueryData, any>(
-        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
-      )
+    () => fetchPosts(pageNumber),
+    {
+      staleTime: 1000 * 60 * 60 * 24,
+    }
   );
 };
+
+export const usePrefetchPosts = (
+  currentPage: CurrentPage,
+  maxPage: typeof maxPostPage
+) => {
+  const queryClient = useQueryClient();
+
+  return useEffect(() => {
+    if (currentPage < maxPage) {
+      const nextPage = currentPage + 1;
+
+      queryClient.prefetchQuery(
+        ["posts", nextPage],
+        () => fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+};
+
+const fetchComments = async (postId: PostId) =>
+  await axios.get<CommentQueryData, any>(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
 
 export const useFetchComments = (
   postId: PostId
 ) => {
   return useQuery<CommentQueryData>(
     ["comments", postId],
-    async () =>
-      await axios.get<CommentQueryData, any>(
-        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-      )
+    () => fetchComments(postId)
   );
 };
 
-export const deletePost = async (
-  postId: PostId
-) => {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
-};
+// export const deletePost = async (
+//   postId: PostId
+// ) => {
+//   const response = await fetch(
+//     `https://jsonplaceholder.typicode.com/postId/${postId}`,
+//     { method: "DELETE" }
+//   );
+//   return response.json();
+// };
 
-export const updatePost = async (
-  postId: PostId
-) => {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    {
-      method: "PATCH",
-      headers: {
-        "Content-type":
-          "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify({
-        title: "REACT QUERY FOREVER!!!!",
-      }),
-    }
-  );
-  return response.json();
-};
+// export const updatePost = async (
+//   postId: PostId
+// ) => {
+//   const response = await fetch(
+//     `https://jsonplaceholder.typicode.com/postId/${postId}`,
+//     {
+//       method: "PATCH",
+//       headers: {
+//         "Content-type":
+//           "application/json; charset=UTF-8",
+//       },
+//       body: JSON.stringify({
+//         title: "REACT QUERY FOREVER!!!!",
+//       }),
+//     }
+//   );
+//   return response.json();
+// };
