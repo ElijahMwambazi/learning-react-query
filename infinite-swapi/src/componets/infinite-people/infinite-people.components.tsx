b63b355a33d227b479c7d12fbeb40396f7ff8262
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-02-07 19:42:44 +0200

    Implemented query of people data

--- infinite-swapi/src/componets/infinite-people/infinite-people.components.tsx
+++ infinite-swapi/src/componets/infinite-people/infinite-people.components.tsx
@@ -1,6 +1,46 @@
+import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteFetchPeople } from "../../api/services/star-wars.api";
+import Person from "../person/person.components";
+
 const InfinitePeople = () => {
-  // TODO: get data for InfiniteScroll via React Query
-  // return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage } =
+    useInfiniteFetchPeople();
+
+  return (
+    <InfiniteScroll
+      loadMore={() => fetchNextPage}
+      hasMore={hasNextPage}
+    >
+      {data &&
+        data.pages.map((pageData) =>
+          pageData.data.results.map(
+            (
+              {
+                name,
+                mass,
+                height,
+                hair_color,
+                eye_color,
+                skin_color,
+                created,
+              },
+              index
+            ) => (
+              <Person
+                key={index}
+                name={name}
+                mass={mass}
+                height={height}
+                hairColor={hair_color}
+                eyeColor={eye_color}
+                skinColor={skin_color}
+                created={created}
+              />
+            )
+          )
+        )}
+    </InfiniteScroll>
+  );
 };
 
 export default InfinitePeople;
--- infinite-swapi/src/componets/person/person.components.tsx
+++ infinite-swapi/src/componets/person/person.components.tsx
@@ -1,20 +1,47 @@
-import { Person } from "../../api/services/star-wars.api";
-
-export type PersonProps = Omit<Person, "id">;
+export type PersonProps = {
+  name: string;
+  mass: string;
+  height: string;
+  eyeColor: string;
+  hairColor: string;
+  skinColor: string;
+  created: string;
+};
 
 const Person = ({
   name,
-  hairColor,
+  height,
+  mass,
   eyeColor,
+  hairColor,
+  skinColor,
+  created,
 }: PersonProps) => {
   return (
-    <li>
-      {name}
+    <div className="person">
+      <h3>{name}</h3>
       <ul>
-        <li>hair: {hairColor}</li>
-        <li>eyes: {eyeColor}</li>
+        <li>
+          <span>height:</span> {height}
+        </li>
+        <li>
+          <span>mass:</span> {mass}
+        </li>
+        <li>
+          <span>eyes:</span> {eyeColor}
+        </li>
+        <li>
+          <span>hair color:</span> {hairColor}
+        </li>
+        <li>
+          <span>skin color:</span> {skinColor}
+        </li>
+        <li>
+          <span>created:</span>{" "}
+          {created.slice(0, 10)}
+        </li>
       </ul>
-    </li>
+    </div>
   );
 };
 
