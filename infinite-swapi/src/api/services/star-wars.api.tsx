acd0fc00925c195e4219fdaeeb8ab74274fb5349
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-02-07 19:41:53 +0200

    Added infinite query of data for people

--- infinite-swapi/src/api/services/star-wars.api.tsx
+++ infinite-swapi/src/api/services/star-wars.api.tsx
@@ -1,36 +1,55 @@
-// const initialUrl =
-//   "https://swapi.dev/api/people/";
-
-// const fetchUrl = async (url) => {
-//   const response = await fetch(url);
-//   return response.json();
-// };
-
-// const initialUrl =
-//   "https://swapi.dev/api/species/";
-
-// const fetchUrl = async (url) => {
-//   const response = await fetch(url);
-//   return response.json();
-// };
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export type Person = {
-  id: number;
   name: string;
-  hairColor: string;
-  eyeColor: string;
+  mass: string;
+  height: string;
+  hair_color: string;
+  eye_color: string;
+  skin_color: string;
+  created: string;
 };
-
 export type Species = {
-  id: number;
   name: string;
   language: string;
   averageLifespan: number;
 };
 
-export type Page = {
+export type PeoplePage = {
   count: number;
   next: string | null;
-  previous: string | null;
-  results: Person[] | Species[];
+  results: Person[];
+};
+export type SpeciesPage = {
+  count: number;
+  next: string | null;
+  results: Species[];
+};
+
+export type PeopleQueryData = {
+  data: PeoplePage;
+};
+export type SpeciesQueryData = {
+  data: SpeciesPage;
+};
+
+const enum APIUrl {
+  INITIAL_PEOPLE_URL = "https://swapi.dev/api/people/",
+  INITIAL_SPECIES_URL = "https://swapi.dev/api/species/",
+}
+
+const fetchPages = async (url: string) =>
+  await axios.get<PeopleQueryData, any>(url);
+
+export const useInfiniteFetchPeople = () => {
+  return useInfiniteQuery<PeopleQueryData>(
+    ["star-war-people"],
+    ({ pageParam = APIUrl.INITIAL_PEOPLE_URL }) =>
+      fetchPages(pageParam),
+    {
+      getNextPageParam: (lastPage) =>
+        lastPage.data.next || null,
+    }
+  );
 };
